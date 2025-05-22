5280faa37202d51c303333e9a06ac607ed7bdccf
Naioku <adrian.komuda.dev@gmail.com>
2025-05-22 19:27:51 +0200

    Added: Dice: Numbers generation.

---
+++ GameTaskAwakenRealms/Assets/Scripts/Dice/Die.cs
@@ -0,0 +1,171 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+#if UNITY_EDITOR
+#endif
+
+namespace Dice
+{
+    public class Die : MonoBehaviour
+    {
+        private const float AngleDegreesTolerance = 1f;
+    
+        [SerializeField] private MeshCollider meshCollider;
+        [SerializeField] private string sideMarkersParentName = "Side markers";
+        [SerializeField, HideInInspector] private Transform sideMarkersParent;
+        [SerializeField] private GameObject sidePrefab;
+        [SerializeField] private List<SideData> sidesData;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (sidesData == null) return;
+            UpdateSides();
+        }
+
+        [ContextMenu("Generate side markers")]
+        private void CMGenerateSideMarkers()
+        {
+            if (!meshCollider)
+            {
+                Debug.LogError("No mesh collider set.");
+                return;
+            }
+        
+            if (!sidePrefab)
+            {
+                Debug.LogError("No side prefab set.");
+                return;
+            }
+
+            List<Triangle> triangleData = CreateTriangleData(meshCollider.sharedMesh);
+            List<List<Triangle>> sides = CreateSides(triangleData);
+        
+            if (sideMarkersParent)
+            {
+                CleanExistingMarkers();
+            }
+
+            GenerateNewMarkers(sides);
+
+            Debug.Log($"{sides.Count} markers generated.");
+        }
+
+        private List<Triangle> CreateTriangleData(Mesh mesh)
+        {
+            Vector3[] vertices = mesh.vertices;
+            int[] triangles = mesh.triangles;
+
+            var triangleData = new List<Triangle>();
+
+            for (int i = 0; i < triangles.Length; i += 3)
+            {
+                Vector3 v0 = transform.TransformPoint(vertices[triangles[i]]);
+                Vector3 v1 = transform.TransformPoint(vertices[triangles[i + 1]]);
+                Vector3 v2 = transform.TransformPoint(vertices[triangles[i + 2]]);
+
+                Vector3 normal = Vector3.Cross(v1 - v0, v2 - v0).normalized;
+                Vector3 center = (v0 + v1 + v2) / 3f;
+
+                triangleData.Add(new Triangle { center = center, normal = normal });
+            }
+
+            return triangleData;
+        }
+
+        private static List<List<Triangle>> CreateSides(List<Triangle> triangleData)
+        {
+            List<List<Triangle>> sideGroups = new List<List<Triangle>>();
+            foreach (Triangle checkedTriangle in triangleData)
+            {
+                bool added = false;
+                foreach (List<Triangle> group in sideGroups)
+                {
+                    Triangle firstTriangleFromGroup = group[0];
+                    float angle = Vector3.Angle(firstTriangleFromGroup.normal, checkedTriangle.normal);
+                    if (angle > AngleDegreesTolerance) continue;
+                
+                    group.Add(checkedTriangle);
+                    added = true;
+                    break;
+                }
+
+                if (!added)
+                {
+                    sideGroups.Add(new List<Triangle> { checkedTriangle });
+                }
+            }
+        
+            return sideGroups;
+        }
+
+        private void CleanExistingMarkers()
+        {
+            DestroyImmediate(sideMarkersParent.gameObject);
+            sidesData.Clear();
+        }
+
+        private void GenerateNewMarkers(List<List<Triangle>> sides)
+        {
+            sideMarkersParent = new GameObject(sideMarkersParentName).transform;
+            sideMarkersParent.transform.SetParent(transform);
+            sideMarkersParent.transform.position = transform.position;
+            sideMarkersParent.transform.rotation = transform.rotation;
+        
+            int sideIndex = 1;
+            foreach (List<Triangle> side in sides)
+            {
+                Vector3 averagePos = Vector3.zero;
+                Vector3 averageNormal = Vector3.zero;
+
+                foreach (Triangle triangle in side)
+                {
+                    averagePos += triangle.center;
+                    averageNormal += triangle.normal;
+                }
+
+                averagePos /= side.Count;
+                averageNormal.Normalize();
+            
+                GameObject marker = (GameObject)PrefabUtility.InstantiatePrefab(sidePrefab, sideMarkersParent);
+                marker.name += $"_{sideIndex}";
+                marker.transform.position = averagePos;
+                marker.transform.rotation = Quaternion.LookRotation(averageNormal);
+                sidesData.Add(new SideData(marker.GetComponent<Side>(), $"{sideIndex}"));
+                sideIndex++;
+            }
+
+            UpdateSides();
+        }
+
+        private void UpdateSides()
+        {
+            foreach (SideData side in sidesData)
+            {
+                side.Update();
+            }
+        }
+
+        private struct Triangle
+        {
+            public Vector3 center;
+            public Vector3 normal;
+        }
+    
+        [System.Serializable]
+        private struct SideData
+        {
+            [SerializeField] private Side side;
+            [SerializeField] private string number;
+
+            public SideData(Side side, string number)
+            {
+                this.side = side;
+                this.number = number;
+            }
+        
+            public void Update() => side.Number = number;
+        }
+#endif
+    }
+}
---
+++ GameTaskAwakenRealms/Assets/Scripts/Dice/Side.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Dice
+{
+    public class Side : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro number;
+
+        public string Number
+        {
+            set
+            {
+                number.text = value;
+                name = name.Split("_")[0] + $"_{value}";
+            }
+        }
+    }
+}
