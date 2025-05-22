dd81a49789adbb413bb49d974646f6e8905e3cb7
Naioku <adrian.komuda.dev@gmail.com>
2025-05-22 22:07:16 +0200

    Added Managers.

---
+++ GameTaskAwakenRealms/Assets/Scripts/Managers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    public static Managers Instance { get; private set; }
+        
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
